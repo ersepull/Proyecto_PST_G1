6e05e709c059504b8b2e13e002c9b1db08ffe2d8
ersepull <ersepull@espol.edu.ec>
2022-01-03 22:24:12 -0500

    Actualización de activities

--- app/src/main/java/com/example/dispetser/Llenado.java
+++ app/src/main/java/com/example/dispetser/Llenado.java
@@ -3,16 +3,24 @@ package com.example.dispetser;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Llenado extends AppCompatActivity {
 
+    Spinner opciones;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_llenado);
+
+        opciones= (Spinner)findViewById(R.id.spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.opciones, android.R.layout.simple_spinner_item);
+        opciones.setAdapter(adapter);
     }
 
     public void volver(View view){
